dedef8fb26eaa00d38c6e38cac527c02a6281a59
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2020-12-02 21:18:01 +0100

    chore: initial commit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,13 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React, { FC } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
-const Layout = ({ children }) => {
+const Layout: FC = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,29 +24,26 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata?.title || 'Title'} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: '0 auto',
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: '0 1.0875rem 1.45rem',
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
+        <footer
+          style={{
+            marginTop: '2rem',
+          }}
+        >
+          © {new Date().getFullYear()}, Built with{' '}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react'
+
+const IndexPage: FC = () => <div>Hello</div>
+
+export default IndexPage
