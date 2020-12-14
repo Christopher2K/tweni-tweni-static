64f520e3f98d8c189cf79856e0633e3096f75ab4
Christopher N. KATOYI <me@christopher2k.dev>
2020-12-14 14:54:22 +0100

    feat: footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import { desktopStyle } from 'styles/responsive'
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  box-sizing: border-box;
+  width: 100%;
+  padding: 3rem ${props => props.theme.nav.padding.sides.mobile} 3rem;
+  font-size: 1.4rem;
+
+  ${props => desktopStyle`
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 3rem;
+    padding: 3rem ${props.theme.nav.padding.sides.desktop} 3rem;
+    margin: 0;
+  `}
+`
+
+const commonTextStyle = css`
+  display: inline-block;
+  font-size: inherit;
+`
+
+const LegalNoticeLink = styled(AniLink)`
+  ${commonTextStyle}
+  color: ${props => props.theme.colors.black};
+`
+
+const Copyright = styled.span`
+  ${commonTextStyle}
+  margin-top: 0.5rem;
+`
+
+export const Footer: FC = () => {
+  return (
+    <Root>
+      <LegalNoticeLink to="/mentions-legales" fade duration={0.4}>
+        Mentions légales
+      </LegalNoticeLink>
+      <Copyright>All rights reserved ©2020</Copyright>
+    </Root>
+  )
+}
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react'
-import { Global } from '@emotion/react'
+import { PageProps } from 'gatsby'
+import { Global, useTheme } from '@emotion/react'
 import styled from '@emotion/styled'
 
 import { Navbar } from 'components/Navbar'
+import { Footer } from 'components/Footer'
 import { global } from 'styles/global'
+import { desktopMediaQuery } from 'styles/responsive'
+import { useWindowSize } from 'hooks/useWindowSize'
+import { useMediaQuery } from 'hooks/useMediaQuery'
+import { toPixels, getLogoHeightForGivenWidth } from 'utils/layout'
 
 const Root = styled.div`
   display: flex;
@@ -15,12 +21,43 @@ const Root = styled.div`
   min-height: 100vh;
 `
 
-export const Layout: FC = ({ children }) => {
+interface PageContainerProps {
+  isHomepage: boolean
+  marginTop: number
+}
+const PageContainer = styled.main<PageContainerProps>`
+  width: 100%;
+  flex: 1;
+  margin-top: ${props => (props.isHomepage ? props.marginTop : 0)}px;
+`
+
+export const Layout: FC<PageProps> = ({ children, location }) => {
+  // Hooks
+  const theme = useTheme()
+  const { width: windowWidth } = useWindowSize()
+  const { match: desktopScreen } = useMediaQuery(`(${desktopMediaQuery})`)
+
+  // Computed
+  const currentContentWidth = windowWidth > 1920 ? 1920 : windowWidth
+  const isHomepage = location.pathname === '/'
+  const _navSidePaddingValue = toPixels(
+    desktopScreen
+      ? theme.nav.padding.sides.desktop
+      : theme.nav.padding.sides.mobile,
+  )
+  const _currentLogoHeight = getLogoHeightForGivenWidth(
+    currentContentWidth - _navSidePaddingValue * 2,
+  )
+  const homepageOffset = _currentLogoHeight + toPixels('2rem')
+
   return (
     <Root>
       <Global styles={global} />
       <Navbar />
-      {children}
+      <PageContainer isHomepage={isHomepage} marginTop={homepageOffset}>
+        {children}
+      </PageContainer>
+      <Footer />
     </Root>
   )
 }
